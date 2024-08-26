a43379f014fbd924aec6cecacf85eaf96792605b
arkim <arkim0630@gmail.com>
2024-08-26 14:47:23 +0900

    redux 세팅

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
 );
 
